b9e32ab6784991f574024aee781cd82bcfdd74f2
M. Castanares <Manu@Ms-MacBook-Air.local>
2019-05-07 15:55:54 -0300

    inicio

--- Tanks.py
+++ Tanks.py
@@ -27,7 +27,7 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
 # Classe Jogador que representa a nave
-class Player(pygame.sprite.Sprite):
+class Tanque_purple(pygame.sprite.Sprite):
     
     # Construtor da classe.
     def __init__(self):
@@ -49,15 +49,17 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         
         # Centraliza embaixo da tela.
-        self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
+        self.rect.x = random.randint(0, 430) 
+        self.rect.y = random.randint(0, 600)
         
         self.speedx = 0
+        self.speedy = 0
         
         self.radius = 25
         
     def update(self):
         self.rect.x += self.speedx
+        self.rect.y += self.speedy
         
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
@@ -78,6 +80,23 @@ pygame.display.set_caption("TANQUE")
 
 clock = pygame.time.Clock()
 
+# Comando para evitar travamentos.
+try:
+    
+    # Loop principal.
+    pygame.mixer.music.play(loops=-1)
+    running = True
+    while running:
+        
+        # Ajusta a velocidade do jogo.
+        clock.tick(FPS)
+        
+        # Processa os eventos (mouse, teclado, botão, etc).
+        for event in pygame.event.get():
+            
+            # Verifica se foi fechado.
+            if event.type == pygame.QUIT:
+                running = False
 
 
 
