173ac1a786d3bec77a245258e4459681fadb14ae
felipe54453 <felipedmlacombe@gmail.com>
2019-05-07 17:23:53 -0300

    bullet img e code

--- Tanks.py
+++ Tanks.py
@@ -119,8 +119,38 @@ class Tanque_green(pygame.sprite.Sprite):
         if self.rect.bottom < 0:
             self.rect.bottom = 0
             
-
+# Classe Bullet que representa os tiros
+class Bullet(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def __init__(self, x, y):
         
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo.
+        bullet_img = pygame.image.load(path.join(img_dir, "laserRed16.png")).convert()
+        self.image = bullet_img
+        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Coloca no lugar inicial definido em x, y do constutor
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    # Metodo que atualiza a posição da navinha
+    def update(self):
+        self.rect.y += self.speedy
+        
+        # Se o tiro passar do inicio da tela, morre.
+        if self.rect.bottom < 0:
+            self.kill()
+
 pygame.init()
 pygame.mixer.init()
 
@@ -157,12 +187,6 @@ try:
             # Verifica se foi fechado.
             if event.type == pygame.QUIT:
                 running = False
-<<<<<<< HEAD
-            
-
-
-
-=======
           # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(background, background_rect)
@@ -170,7 +194,6 @@ try:
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
         
->>>>>>> 186889501f7aeb6e8e4c63a595adc600158d1114
 finally:
     
     pygame.quit()
