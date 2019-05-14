2fb9026241e31febca9c7bde3122447f127e39f7
felipe54453 <45129917+felipe54453@users.noreply.github.com>
2019-05-14 15:58:47 -0300

    Bullet

--- Tanks.py
+++ Tanks.py
@@ -156,32 +156,32 @@ class Tanque_green(pygame.sprite.Sprite):
             self.rect.bottom = 0
             
 # Classe Bullet que representa os tiros
-#class Bullet(pygame.sprite.Sprite):
+class Bullet(pygame.sprite.Sprite):
     
     # Construtor da classe.
-    #def __init__(self, x, y):
+    def __init__(self, x, y):
         
         # Construtor da classe pai (Sprite).
-        #pygame.sprite.Sprite.__init__(self)
+        pygame.sprite.Sprite.__init__(self)
         
         # Carregando a imagem de fundo.
-        #Bullet_img = pygame.image.load(path.join(img_dir, "Bullet.png")).convert()
-        #self.image = Bullet_img
+        Bullet_img = pygame.image.load(path.join(img_dir, "Bullet.png")).convert()
+        self.image = Bullet_img
         
         # Deixando transparente.
-        #self.image.set_colorkey(BLACK)
+        self.image.set_colorkey(BLACK)
         
         # Detalhes sobre o posicionamento.
-        #self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect()
         
         # Coloca no lugar inicial definido em x, y do constutor
-        #self.rect.bottom = y
-        #self.rect.centerx = x
-        #self.speedy = -10
-
+        self.rect.centery = y
+        self.rect.centerx = x
+        self.speedy = -10
+        
     # Metodo que atualiza a posição da navinha
-    #def update(self):
-        #self.rect.y += self.speedy
+    def update(self):
+        self.rect.y += self.speedy
         
         # Se o tiro passar do inicio da tela, morre.
         #if self.rect.bottom < 0:
@@ -205,6 +205,7 @@ player2 = Tanque_green()
 #bullet = Bullet()
 # Cria um grupo de todos os sprites e adiciona a nave.
 all_sprites = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 all_sprites.add(player1)
 all_sprites.add(player2)
 #all_sprites.add(bullet)
@@ -267,10 +268,10 @@ try:
                 if event.key == pygame.K_s:
                     player2.speed = 1.5
                     
-               # if event.key == pygame.K_SPACE:
-                        #bullet = Bullet(player1.rect.centerx, player1.rect.top, pygame.image.load(path.join(img_dir, "Bullet.png")).convert())
-                        #all_sprites.add(bullet)
-                        #bullet.add(bullet)
+                if event.key == pygame.K_SPACE:
+                        bullet = Bullet(player1.rect.centerx, player1.rect.centery)
+                        bullets.add(bullet)
+                        all_sprites.add(bullets)
                    
             if event.type == pygame.KEYUP:
                 # Dependendo da tecla, altera a velocidade.
@@ -318,4 +319,3 @@ try:
 finally:
     
     pygame.quit()
-
